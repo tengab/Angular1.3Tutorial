06c71648acb2589a5f17a4c7ffe2646d072ea285
Tomasz Banach <tomasz.banach@britenet.com.pl>
2018-06-27 12:37:41 +0200

    $http

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var myApp = angular.module('myApp', ['ngMessages', 'ngResource'])
 
-myApp.controller('mainController', ['$scope', '$filter', '$timeout', function ($scope, $filter, $timeout) {
+myApp.controller('mainController', ['$scope', '$filter', '$timeout', '$http', function ($scope, $filter, $timeout, $http) {
 
     $scope.handle = ''
 
@@ -29,6 +29,30 @@ myApp.controller('mainController', ['$scope', '$filter', '$timeout', function ($
     }
 
     $scope.name = 'Tomek'
+
+    $http.get('https://randomuser.me/api/?results=5')
+        .success(function (result) {
+            console.log(result.results)
+            $scope.rules = result.results
+        })
+        .error(function (data, status) {
+            console.log('data ' + data)
+            console.log('status ' + status)
+        })
+
+        $scope.newRule = ''
+        $scope.addRule = function() {
+
+            $http.post('https://tomaszbanach01-sandbox.firebaseio.com/homeworkTaskList', {name : $scope.newRule})
+            .success(function(result) {
+                $scope.rules = result
+                $scope.newRule = ''  //clear textbox
+            })
+            .error(function(data, status){
+                console.log('ERROR' + data)
+            })
+
+        }
 
 }])
 
