0a0f63a3abeb56e22f05e131ff14f74aee192020
Tomasz Banach <tomasz.banach@britenet.com.pl>
2018-06-27 09:38:30 +0200

    basic directives: ng-show, ng-hide, ng-if, ng-class, ng-repeat

--- app.js
+++ app.js
@@ -14,7 +14,15 @@ myApp.controller('mainController', ['$scope', '$filter', '$timeout', function ($
         console.log('new:' + newValue)
     })
 
+    $scope.characters = 5
 
+    $scope.rules = [
+        {ruleName: 'Must be 5 characters'},
+        {ruleName: 'Must not be used elsewhere'},
+        {ruleName: 'Must be cool'}
+    ]
+
+console.log($scope.rules)
 
 }])
 
